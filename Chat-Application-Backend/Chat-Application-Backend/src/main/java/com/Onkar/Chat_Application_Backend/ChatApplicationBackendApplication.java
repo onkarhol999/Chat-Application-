cfb8ea61b0dbb8357f84a112dbc248cb6d744f53
Onkar hol <holonkar2003@gmail.com>
2025-03-20 15:40:29 +0530

    Project Start backend system start

---
+++ Chat-Application-Backend/Chat-Application-Backend/src/main/java/com/Onkar/Chat_Application_Backend/ChatApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Onkar.Chat_Application_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplicationBackendApplication.class, args);
+	}
+
+}
---
+++ Chat-Application-Backend/Chat-Application-Backend/src/main/java/com/Onkar/Chat_Application_Backend/entities/Room.java
@@ -0,0 +1,4 @@
+package com.Onkar.Chat_Application_Backend.entities;
+
+public class Room {
+}
